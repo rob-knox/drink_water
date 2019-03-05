71870d0c6f9b7a1dedc4a90e0ba862edd742faef
Ken Oak <kenoak@gmail.com>
2019-03-04 17:03:33 -0800

    seventh commit

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Facts } from './models/Facts';
 
 @Component({
   selector: 'app-root',
@@ -7,35 +6,14 @@ import { Facts } from './models/Facts';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  facts: Facts[];
   weight: number;
   min: any;
+
     
   constructor() {}
 
   ngOnInit() {
-    this.facts = [
-      {
-        id: 1,
-        tip: 'Tip 1',
-      },
-      {
-        id: 2,
-        tip: 'Tip 2',
-      },
-      {
-        id: 3,
-        tip: 'Tip 3',
-      }
-    ]
-    // Cycle through facts
-    let slides = this.facts;
-    let currentSlide = 0;
-    let slideInterval = setInterval(nextSlide, 3000);
-    function nextSlide() {
-      currentSlide = (currentSlide+1)%slides.length;
-      slides[currentSlide].tip;
-    }
+
 
   }
   onSubmit() {
@@ -46,6 +24,5 @@ export class AppComponent implements OnInit {
     }, minutes);
   }
 
-  inputChange(min:string):void{this.min=min}
 
 }
